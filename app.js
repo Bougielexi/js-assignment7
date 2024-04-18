00a172f5ebe53d7c29f6f31d2a92112f8766387e
Lexi Bougie <300028074@fvtc.edu>
2024-04-18 16:52:25 -0500

    assignment 7

--- app.js
+++ app.js
@@ -17,14 +17,28 @@ app.get('/', (_, response) => {
 	response.sendFile('index.html', { root })
 })
 
-
-
 // GET /api/todos
+app.get('/api/todos', (request, response) => {
+	response.json(todos)
+})
 
-// POST /api/todos
+  // POST /api/todos
+app.post('/api/todos', (request, response) => {
+	const { item } = request.body;
+	const id = todos.length + 1;
+	const complete = false;
+	todos.push({ id, item, complete });
+	response.json({id});
+  });
 
 // PUT /api/todos/:id
-
+app.put('/api/todos/:id', (request, response) => {
+	const { id } = request.params;
+	const task = todos.find(todo => todo.id.toString() === id)
+	task.complete = !task.complete;
+	const info = { id: task.id, complete: task.complete };
+	response.send(info);
+});
 
 
 const message = `Server running: http://localhost:${port}`
